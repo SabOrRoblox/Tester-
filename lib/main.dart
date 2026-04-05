6dba46cde328b98a94770d383e13e7a3a7d3fcba
SabOrRoblox <maksymorigonal2013@gmail.com>
2026-04-05 20:14:03 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:confetti/confetti.dart';
 import 'dart:async';
 
